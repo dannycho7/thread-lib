75f910861b867e8af82205ab3a2747d36b63262a
dannycho7 <dannycho7@gmail.com>
2019-02-10 09:59:21 -0800

    Use fixed size arr to hold TCB

--- thread_manager.cpp
+++ thread_manager.cpp
@@ -24,7 +24,8 @@ static int ptr_mangle(int p) {
 }
 
 void alarm_handler(int signo) {
-	if (setjmp(ThreadManager::get().getRunningTCB()->buf) == 0) {
+	if (setjmp(ThreadManager::get().getRunningTCB().buf) == 0) {
+		ThreadManager::get().getRunningTCB().state = READY;
 		ThreadManager::get().nextThread();
 	}
 }
@@ -47,67 +48,63 @@ void initTimer() {
 		perror("error calling setitimer()");
 		exit(1);
 	}
+
 }
 
 ThreadManager::ThreadManager()
-: highest_thread_id{0}
-, mapTCB()
-, running_thread_it() {
-	TCB* curr_tcb = new TCB(highest_thread_id); /* main thread receives id 0 */
-	setjmp(curr_tcb->buf);
-	this->mapTCB[curr_tcb->thread_id] = curr_tcb;
-	running_thread_it = this->mapTCB.begin();
+: num_threads{0}
+, curr_thread_i{0} {
+	TCB curr_tcb(num_threads); /* main thread receives id 0 */
+	curr_tcb.state = RUNNING;
+	setjmp(curr_tcb.buf);
+	this->tcb_arr[num_threads] = curr_tcb;
+	num_threads++;
 	initTimer();
 }
 
-ThreadManager::~ThreadManager() {
-	for (auto it = this->mapTCB.begin(); it != this->mapTCB.end(); it++) {
-		delete it->second;
-	}
-}
-
 void ThreadManager::createThread(pthread_t* thread, const pthread_attr_t* attr, void* (*start_routine)(void *), void *arg, void (*exit_addr)(void *)) {
-	*thread = ++(this->highest_thread_id);
-	TCB* crt_tcb = new TCB(*thread);
+	*thread = this->num_threads;
+	TCB crt_tcb(*thread);
+	setjmp(crt_tcb.buf);
 
-	setjmp(crt_tcb->buf);
-
-	uint8_t* stack_ptr = ((uint8_t *) (crt_tcb->stack_top));
+	uint8_t* stack_ptr = ((uint8_t *) (crt_tcb.stack_top));
 
 	*((int *) (stack_ptr + STACK_SIZE - 1 * sizeof(int))) = (intptr_t) arg;
 	*((int *) (stack_ptr + STACK_SIZE - 2 * sizeof(int))) = (intptr_t) exit_addr;
 
-	crt_tcb->buf->__jmpbuf[JB_SP] = ptr_mangle((intptr_t)(stack_ptr + STACK_SIZE - 2 * sizeof(int)));
-	crt_tcb->buf->__jmpbuf[JB_PC] = ptr_mangle((intptr_t)(start_routine));
+	crt_tcb.buf->__jmpbuf[JB_SP] = ptr_mangle((intptr_t)(stack_ptr + STACK_SIZE - 2 * sizeof(int)));
+	crt_tcb.buf->__jmpbuf[JB_PC] = ptr_mangle((intptr_t)(start_routine));
+	crt_tcb.state = READY;
 
-	this->mapTCB[crt_tcb->thread_id] = crt_tcb;
+	this->tcb_arr[num_threads] = crt_tcb;
+	num_threads++;
 }
 
 [[ noreturn ]] void ThreadManager::finishCurrentThread() {
-	pthread_t curr_thread_id = this->running_thread_it->second->thread_id;
-	TCB* fin_tcb = this->mapTCB.find(curr_thread_id)->second;
-	delete fin_tcb;
-	this->running_thread_it = this->mapTCB.erase(this->running_thread_it);
-	if (this->running_thread_it == this->mapTCB.end())
-		this->running_thread_it = this->mapTCB.begin();
-	if (this->mapTCB.size() == 0)
-		exit(0);
-	longjmp(this->running_thread_it->second->buf, 1);
+	TCB fin_tcb = this->tcb_arr[curr_thread_i];
+	free(fin_tcb.stack_top);
+	fin_tcb.state = TERMINATED;
+	this->nextThread();
 }
 
-TCB* ThreadManager::getRunningTCB() { return this->mapTCB[this->running_thread_it->second->thread_id]; }
+TCB& ThreadManager::getRunningTCB() { return this->tcb_arr[curr_thread_i]; }
 
-/* Invariant is that the running_thread_it is an iterator to a valid value */
 [[ noreturn ]] void ThreadManager::nextThread() {
-	assert(this->mapTCB.size() > 0);
-	this->running_thread_it++;
-	if (this->running_thread_it == this->mapTCB.end())
-		this->running_thread_it = this->mapTCB.begin();
-	longjmp(this->running_thread_it->second->buf, 1);
+	int i;
+	for (i = 0; i < this->num_threads; i++) {
+		this->curr_thread_i = (this->curr_thread_i + 1) % this->num_threads;
+		if (this->tcb_arr[curr_thread_i].state == READY)
+			break;
+	}
+	if (i == this->num_threads)
+		exit(0);
+
+	this->tcb_arr[curr_thread_i].state = RUNNING;
+	longjmp(this->tcb_arr[curr_thread_i].buf, 1);
 } 
 
 pthread_t pthread_self() {
-	return ThreadManager::get().getRunningTCB()->thread_id;
+	return ThreadManager::get().getRunningTCB().thread_id;
 }
 
 void pthread_exit(void* value_ptr) {
--- thread_manager.hpp
+++ thread_manager.hpp
@@ -1,40 +1,40 @@
 #ifndef THREAD_MANAGER_H
 #define THREAD_MANAGER_H
 #define STACK_SIZE 32767
+#define MAX_THREADS 128
 
 #include <map>
 #include <pthread.h>
 #include <setjmp.h>
 
+enum TCB_STATE { RUNNING, READY, TERMINATED };
+
 struct TCB {
 	pthread_t thread_id;
 	int* stack_top;
 	jmp_buf buf;
+	TCB_STATE state;
+	TCB() {} /* try to remove?? */
 	TCB(pthread_t thread_id) {
 		this->thread_id = thread_id;
-		int *stack = (int *) malloc(STACK_SIZE);
+		int* stack = (int *) malloc(STACK_SIZE);
 		this->stack_top = stack;
-	}
-	~TCB() {
-		free(this->stack_top);
 	}
 };
 
 class ThreadManager {
 public:
-	~ThreadManager();
 	static ThreadManager& get() {
 		static ThreadManager tm;
 		return tm;
 	}
 	void createThread(pthread_t* thread, const pthread_attr_t* attr, void* (*start_routine)(void *), void *arg, void (*exit_addr)(void *));
 	[[ noreturn ]] void finishCurrentThread();
-	TCB* getRunningTCB();
+	TCB& getRunningTCB();
 	[[ noreturn ]] void nextThread();
 private:
 	ThreadManager();
-	pthread_t highest_thread_id;
-	std::map<pthread_t, TCB*> mapTCB;
-	std::map<pthread_t, TCB*>::iterator running_thread_it;
+	int num_threads, curr_thread_i;
+	TCB tcb_arr[MAX_THREADS];
 };
 #endif
